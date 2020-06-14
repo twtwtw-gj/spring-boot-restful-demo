089b1fb9b1e3939e28a6d623e2b48d902c081ca5
twtwtw-gj <64819540+twtwtw-gj@users.noreply.github.com>
2020-06-14 18:01:44 +0900

    made TestController
    テスト用のcontrollerを作成

---
+++ src/test/java/com/twtwtw/springbootrestfuldemo/SpringBootRestfulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twtwtw.springbootrestfuldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
